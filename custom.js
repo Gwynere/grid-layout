2153f91dd351ed17901d67bca2b69786a66c62aa
= <=>
2018-10-03 11:23:33 +0200

    proejct finished

--- custom.js
+++ custom.js
@@ -4,3 +4,17 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+// STICKY NAVIGATION BAR
+var nav = document.getElementById('nav');
+var topNav = nav.offsetTop;
+
+function fixedNav(){
+  if(window.scrollY > topNav){
+    document.body.classList.add('fixed-nav');
+  } else{
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+window.addEventListener('scroll', fixedNav);
